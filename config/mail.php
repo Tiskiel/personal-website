82d0efdd8bbffb55260fc9bed31cea08e192fdc2
Wanty Jean-François <wanty.jf@gmail.com>
2023-09-25 16:32:19 +0200

    Implement resend

--- config/mail.php
+++ config/mail.php
@@ -64,6 +64,10 @@ return [
             // ],
         ],
 
+        'resend' => [
+            'transport' => 'resend',
+        ],
+
         'sendmail' => [
             'transport' => 'sendmail',
             'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
