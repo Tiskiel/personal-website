c906c97b0e5887ac26b02e999ed669d6c467b1aa
Wanty Jean-François <wanty.jf@gmail.com>
2023-09-25 22:59:42 +0200

    Starting the home page implementation

--- resources/js/Layouts/HomeLayout.tsx
+++ resources/js/Layouts/HomeLayout.tsx
@@ -1,5 +1,5 @@
-import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/20/solid';
 import { PropsWithChildren } from 'react';
+import HomeFooter from './Components/HomeFooter';
 
 const profile = {
   name: 'Jean-François Wanty',
@@ -17,48 +17,35 @@ const profile = {
 export default function HomeLayout({ children }: PropsWithChildren) {
   return (
     <div>
-      <div>
+      <div className="fixed top-0 w-full bg-white z-10">
         <div>
-          <img className="h-32 w-full object-cover lg:h-48" src={profile.backgroundImage} alt="" />
-        </div>
-        <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-          <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-            <div className="flex">
-              <img
-                className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32 object-cover"
-                src={profile.avatar}
-                alt=""
-              />
-            </div>
-            <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
-              <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
-                <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
-                <h3 className="text-sm font-medium text-gray-500">{profile.fields[1][1]}</h3>
-              </div>
-              <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
-                <button
-                  type="button"
-                  className="inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  <EnvelopeIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                  <span>Message</span>
-                </button>
-                <button
-                  type="button"
-                  className="inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  <PhoneIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                  <span>Call</span>
-                </button>
-              </div>
-            </div>
+          <div>
+            <img className="h-32 w-full object-cover lg:h-48" src={profile.backgroundImage} alt="" />
           </div>
-          <div className="mt-6 hidden min-w-0 flex-1 sm:block md:hidden">
-            <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
+          <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+            <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+              <div className="flex">
+                <img
+                  className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32 object-cover"
+                  src={profile.avatar}
+                  alt=""
+                />
+              </div>
+              <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
+                  <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
+                  <h3 className="text-sm font-medium text-gray-500">{profile.fields[1][1]}</h3>
+                </div>
+              </div>
+            </div>
+            <div className="mt-6 hidden min-w-0 flex-1 sm:block md:hidden">
+              <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
+            </div>
           </div>
         </div>
       </div>
-      <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">{children}</div>
+      <div className="mt-80 mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">{children}</div>
+      <HomeFooter />
     </div>
   );
 }
--- resources/js/Pages/Home/Index.tsx
+++ resources/js/Pages/Home/Index.tsx
@@ -2,7 +2,51 @@ import HomeLayout from '@/Layouts/HomeLayout';
 import { ReactNode } from 'react';
 
 function Index() {
-  return 'Hello world';
+  return (
+    <div className="grid grid-cols-[2fr,1fr] gap-5 p-4">
+      <main className="bg-slate-200 rounded-md p-4 flex flex-col justify-center items-center min-h-screen">
+        <div className="max-w-xl w-full">
+          <p className="text-lg mb-4">
+            Following a career change, I turned towards web-oriented programming training. It may seem quite simple, but
+            I redirected myself to this sector because I have always loved video games and the world of computers.
+          </p>
+
+          <p className="text-lg mb-4">
+            During my training, I had the opportunity to discover several languages, such as C#, Javascript, Typescript,
+            Kotlin, and Java. I also got to explore frameworks like React, Angular, and Express, to name a few.
+          </p>
+
+          <p className="text-lg mb-4">
+            Afterward, I was fortunate to be hired by Gitscape, where I am still currently working. In this young
+            company, I was able to discover a new language and a new framework.
+          </p>
+
+          <p className="text-lg mb-4">
+            As I write this emphasis, my tech stack comprises Laravel, Inertia, React, and Typescript, and I am
+            experiencing the joys of devops and GitHub Actions.
+          </p>
+
+          <p className="text-lg">Thank you for taking the time to read me.</p>
+        </div>
+      </main>
+
+      <aside className="bg-slate-100 rounded-md p-4 flex flex-col justify-center">
+        <p className="text-black text-lg mb-4">
+          I am someone passionate and curious. I really enjoy learning and tackling challenges.
+        </p>
+
+        <p className="text-black text-lg mb-4">
+          I love working both in a team and solo, with a slight preference for the team to stay connected.
+        </p>
+
+        <p className="text-black text-lg">
+          Outside of coding, I love strolling in nature and exploring new horizons with my family.
+        </p>
+      </aside>
+
+      <div className="bg-red-200 rounded-md p-4 flex flex-col justify-center">test</div>
+    </div>
+  );
 }
 
 Index.layout = (page: ReactNode) => <HomeLayout>{page}</HomeLayout>;
