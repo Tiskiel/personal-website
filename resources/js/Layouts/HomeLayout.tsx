7fc6eb30e0b80951adbc2cbcd9900d8c8fb59936
Wanty Jean-François <wanty.jf@gmail.com>
2023-09-25 21:52:16 +0200

    Improve header

--- resources/js/Layouts/HomeLayout.tsx
+++ resources/js/Layouts/HomeLayout.tsx
@@ -1,14 +1,12 @@
 import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/20/solid';
+import { PropsWithChildren } from 'react';
 
 const profile = {
   name: 'Jean-François Wanty',
   email: 'wanty.jf@gmail.com',
-  avatar:
-    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-  backgroundImage:
-    'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+  avatar: '/images/DSC_0273.JPG',
+  backgroundImage: '/images/20220808_190627.jpg',
   fields: [
-    ['Phone', '+32 496/36.62.59'],
     ['Email', 'wanty.jf@gmail.com'],
     ['Title', 'Junior full-stack developer'],
     ['Location', 'Belgium'],
@@ -16,7 +14,7 @@ const profile = {
   ],
 };
 
-export default function Example() {
+export default function HomeLayout({ children }: PropsWithChildren) {
   return (
     <div>
       <div>
@@ -26,11 +24,16 @@ export default function Example() {
         <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
           <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
             <div className="flex">
-              <img className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32" src={profile.avatar} alt="" />
+              <img
+                className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32 object-cover"
+                src={profile.avatar}
+                alt=""
+              />
             </div>
             <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
               <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
                 <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
+                <h3 className="text-sm font-medium text-gray-500">{profile.fields[1][1]}</h3>
               </div>
               <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
                 <button
--- resources/js/Pages/Home/Index.tsx
+++ resources/js/Pages/Home/Index.tsx
@@ -1,3 +1,9 @@
-export default function Index() {
-  return <div>Index</div>;
+import HomeLayout from '@/Layouts/HomeLayout';
+import { ReactNode } from 'react';
+
+function Index() {
+  return 'Hello world';
 }
+
+Index.layout = (page: ReactNode) => <HomeLayout>{page}</HomeLayout>;
+export default Index;
