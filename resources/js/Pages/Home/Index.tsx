c034d1e563c98f83ef36232518d9b931ac8d726c
Wanty Jean-François <wanty.jf@gmail.com>
2023-10-10 18:47:39 +0200

    Move the project view to a component

--- resources/js/Pages/Home/Index.tsx
+++ resources/js/Pages/Home/Index.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/sha
 import { Link } from '@inertiajs/react';
 import { GitHubLogoIcon, VercelLogoIcon } from '@radix-ui/react-icons';
 import { ReactNode } from 'react';
+import ProjectShow from './Components/ProjectShow';
 
 function Index() {
   return (
@@ -48,35 +49,7 @@ function Index() {
         </p>
       </aside>
 
-      <div className="bg-slate-50 rounded-md p-4 flex flex-col justify-center">
-        <h3 className="text-center mb-2 underline text-lg">My projects</h3>
-        <div className="max-w-sm rounded-md overflow-hidden shadow-lg">
-          <img className="w-full" src="/images/Screenshot 2023-09-26 at 10.58.04.png" alt="pokemon finder" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">Pokemon finder</div>
-            <p className="text-gray-700 text-base">
-              This is my first website created with React and used the pokemon API.
-            </p>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              <a href="https://github.com/Tiskiel/pokemonFinder">
-                <GitHubLogoIcon className="w-6 h-6" />
-              </a>
-            </span>
-            <span className="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              <a href="https://pokemonfinder-ivory.vercel.app/home">
-                <VercelLogoIcon className="w-6 h-6" />
-              </a>
-            </span>
-          </div>
-        </div>
-        <div className="text-center mt-2">
-          <Link href="">
-            <PrimaryButton>See more</PrimaryButton>
-          </Link>
-        </div>
-      </div>
+      <ProjectShow />
     </div>
   );
 }
