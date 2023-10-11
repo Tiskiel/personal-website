a2ce55bf7d9a2024ce1dbb3b2c44138526e95323
Wanty Jean-François <wanty.jf@gmail.com>
2023-10-11 14:23:24 +0200

    Improving the card

--- resources/js/Components/Card.tsx
+++ resources/js/Components/Card.tsx
@@ -1,3 +1,6 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+
 export type logos = {
   link: string;
   icon: JSX.Element;
@@ -14,19 +17,26 @@ export type CardProps = {
 };
 
 export default function Card({ title, description, image, logos }: CardProps) {
+  const [isTruncated, setIsTruncated] = useState(true);
+  const truncate = clsx({ truncate: isTruncated });
   return (
     <div className="max-w-sm rounded-md overflow-hidden shadow-lg">
       <img className="w-full" src={image.src} alt={image.alt} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">{description}</p>
+        <p className={`text-gray-700 text-base ${truncate}`}>{description}</p>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        {logos.map(logo => (
-          <span className="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            <a href={logo.link}>{logo.icon}</a>
-          </span>
-        ))}
+      <div className="flex px-6 pt-4 pb-2">
+        <div className="basis-2/3">
+          {logos.map(logo => (
+            <span className="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              <a href={logo.link}>{logo.icon}</a>
+            </span>
+          ))}
+        </div>
+        <button className="rounded-xl border p-2 hover:border-blue-500" onClick={() => setIsTruncated(!isTruncated)}>
+          {isTruncated ? 'Read more' : 'Read less'}
+        </button>
       </div>
     </div>
   );
