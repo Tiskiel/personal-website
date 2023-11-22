c3f218b184cfc3ffd10939ca23bf684330c16676
Wanty Jean-François <wanty.jf@gmail.com>
2023-11-22 09:52:28 +0100

    Fix footer navigation

--- resources/js/Layouts/Components/HomeFooter.tsx
+++ resources/js/Layouts/Components/HomeFooter.tsx
@@ -44,7 +44,7 @@ export default function HomeFooter() {
   return (
     <footer className="bg-white">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-        <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+        <nav className="-mb-6  flex gap-6 justify-between mx-8 sm:justify-center sm:gap-x-12" aria-label="Footer">
           {navigation.main.map(item => (
             <div key={item.name} className="pb-6">
               <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
