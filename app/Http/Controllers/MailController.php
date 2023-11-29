5852d22b38e419000422195e8a5bbddcf20975fe
Wanty Jean-François <wanty.jf@gmail.com>
2023-11-29 09:50:50 +0100

    Change the mail

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,8 +10,8 @@ class MailController extends Controller
     public function __invoke(MailFormRequestData $data)
     {
         Resend::emails()->send([
-            'from' => 'Acme <onboarding@resend.dev>',
-            'to' => $data->email,
+            'from' => $data->email,
+            'to' => 'wanty.jf@gmail.com',
             'subject' => 'New mail from'.' '.$data->company ?? $data->first_name,
             'html' => '<p>Hi I\'m'.' '.$data->first_name.' '.$data->last_name.',</p><p>'.$data->message.'</p>',
         ]);
